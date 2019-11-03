3053769d4d7488ce3f599092d97e1568bbe9dab4
unknown <maibeeher@mail.fresnostate.edu>
2019-11-03 04:55:46 -0800

    finish lab 5

--- src/app/main-header/main-header.component.spec.ts
+++ src/app/main-header/main-header.component.spec.ts
@@ -8,9 +8,9 @@ describe('MainHeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainHeaderComponent ]
+      declarations: [MainHeaderComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +22,5 @@ describe('MainHeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+}); import { from } from 'rxjs';
+
